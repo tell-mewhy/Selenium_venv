1531f832ef4dd06ccd312abfed5d6fa1568e7ac0
tell-mewhy <zielony.alien@gmail.com>
2021-02-18 22:56:55 +0100

    some changes

--- testy/test_case.py
+++ testy/test_case.py
@@ -1,1 +1,10 @@
-print('Hello world')
+import unittest
+
+from selenium import webdriver
+from webdriver_manager.firefox import GeckoDriverManager
+
+driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+
+class TestCase(unittest.TestCase):
+    def setUp(self):
+        self.driver = driver
